40d9c9d70dfab9a4b6ac9e1c006c9ca35334dd58
Ron Stuchevsky <50965515+ronKzl@users.noreply.github.com>
2022-05-19 05:20:10 -0400

    Update Part.h

--- Part.h
+++ Part.h
@@ -34,44 +34,5 @@ class Part {
 };
 //****************** PART CLASS END ********************************
 
-// //****************** FH_PART CLASS  START ********************************
-// class FH_Part: virtual public Part {
-//   public:
-//     FH_Part(const string& name, int number);
-//     virtual ~FH_Part();
-//     virtual bool inspection(Date&);
-//     virtual void print(ostream& os) const;
-//   protected:
-//     int fh_inspect;
-//
-// };
-// //****************** FH_PART CLASS END********************************
-
-// //****************** IT_PART CLASS  START ********************************
-// class IT_Part: virtual public Part {
-//   public:
-//     IT_Part(const string& name, int number);
-//     virtual ~IT_Part();
-//     virtual bool inspection(Date&);
-//     virtual void print(ostream& os) const;
-//   protected:
-//     int it_inspect;
-//
-// };
-// //****************** IT_PART CLASS  END ********************************
-
-// //****************** FHIT_PART CLASS  START ********************************
-//
-// class FHIT_Part: public FH_Part, public IT_Part {
-//   public:
-//     FHIT_Part(const string& name,int fh_inspect, int it_inspect);
-//     virtual ~FHIT_Part();
-//     virtual bool inspection(Date&);
-//     virtual void print(ostream& os) const;
-//
-// };
-//
-// //****************** FHIT_PART CLASS  END ********************************
-
 
 #endif
